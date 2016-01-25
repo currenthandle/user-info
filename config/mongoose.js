162ccdf4a04de4d45481d69bef7212b424aa0fa6
leptone <leptone55@gmail.com>
2016-01-25 14:46:06 -0800

    Configured app for deployment / production

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose')
-
 module.exports = function() {
-	var db = mongoose.connect('mongodb://localhost/explore-mean')
-	
+	var db
+	if(process.env.NODE_ENV === 'development'){
+		mongoose.connect('mongodb://localhost/explore-mean')
+	} else { 
+		mongoose.connect('mongodb://leptone:leptone@ds033145.mongolab.com:33145/heroku_n1twfcxv')
+	}	
 	require('../app/models/user')
 	
 	return db
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
-	morgan = require('morgan')
+	morgan = require('morgan'),
 	mongoose = require('./config/mongoose.js'),
 	db = mongoose()
 
-app.use(morgan('dev'))
-
+	if(process.env.NODE_ENV === 'development'){
+		app.use(morgan('dev'))
+	} else {
+		app.use(compression())	
+	}
 app.use(express.static(__dirname + '/public'))
 
 app.use(bodyParser.urlencoded())
